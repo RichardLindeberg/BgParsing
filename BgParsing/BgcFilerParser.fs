8535ac530384cc2216d07a8c18685e8745ab72e8
Richard Lindeberg <richard.lindeberg@qliro.com>
2016-11-25 20:53:12 +0100

    ..

--- BgParsing/BgcFilerParser.fs
+++ BgParsing/BgcFilerParser.fs
@@ -14,10 +14,7 @@ let parseFile p fileName =
     | Success(result, _, _)   ->  Passed <| (fileName, result)
     | Failure(errorMsg, pe, us) -> Failed <| sprintf "Failure on file: %s, \n\n Error:  %s \n\n pe: %A \n\n userState: %A" fileName errorMsg pe us
 
-let files = System.IO.Directory.GetFiles(@"C:\temp\BG\joined")
-
 open BgParsing.Models.SharedParsingTypes
-
 
 let loadFiles (files : string []) = 
     let fileToEvents = 
